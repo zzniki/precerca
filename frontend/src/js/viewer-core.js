dc2b6027fbfc4389169c78176a00af5f3081cec3
iByNiki <39004156+iByNiki@users.noreply.github.com>
2022-10-10 20:56:01 +0200

    Update viewer-core.js

--- frontend/src/js/viewer-core.js
+++ frontend/src/js/viewer-core.js
@@ -51,6 +51,7 @@ async function loop(model) {
 
         output = output.slice(0, -1);
 
+        console.log(predictions[0].boundingBox);
         console.log(output);
         socket.send(output);
 
